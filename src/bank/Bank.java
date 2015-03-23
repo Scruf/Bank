2fbbb6e8394af12326a770ef361b4c9b1447c0a1
Scruf <egor.kozitski@mail.citytech.cuny.edu>
2015-03-23 11:09:43 -0400

    first

---
+++ src/bank/Bank.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bank;
+
+/**
+ *
+ * @author Egor
+ */
+public class Bank {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       MainScreen main = new MainScreen();
+       main.setVisible(true);
+    }
+    
+}
